8cffe4fa44d0be4623afa5f7b68d27adb85a6cc2
Vladislav Georgiev <vlad.georgiev@outlook.com>
2019-03-25 10:15:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var arr = []
+function takeANumber(arr, newCustomer) {
 
-function takeANumber (arr, newCustomer) {
+  var arr = []
+
   arr.push(newCustomer)
   return `Welcome, ${newCustomer}. You are number ${arr.length} in line.`
 }
 
-function nowServing (arr) {
+function nowServing(arr) {
+
+  var arr = []
 
   if (!arr.length) {
     return 'There is nobody waiting to be served!'
@@ -14,13 +17,14 @@ function nowServing (arr) {
   return `Currently serving ${arr.shift()}.`
 }
 
-function currentLine (arr) {
+function currentLine(arr) {
 
   if (!arr.length) {
     return "The line is currently empty."
   }
 
   var arrNew = []
+
   for (var i = 0; i < arr.length; i++) {
     arrNew.push(` ${ i + 1}. ${arr[i]}`)
   }
